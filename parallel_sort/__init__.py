465e4836b1d23901315b2548f33aecb9247fba92
Calvin Sykes <calvin.v.sykes@durham.ac.uk>
2021-08-03 22:56:48 +0100

    packaging works now?

--- parallel_sort/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Parallel_sort package"""
-from .extension import (
-    parallel_sort, parallel_sort_inplace, parallel_argsort
-)
--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-from setuptools import Extension, setup, find_packages
+from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize, build_ext
 
 import numpy as np
 
-ext = Extension(
-    "parallel_sort.extension",
-    sources=["parallel_sort/extension.pyx"],
-    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-    extra_compile_args=["-fopenmp"],
-    extra_link_args=["-liomp5", "-lpthread"],
-    include_dirs=[np.get_include()],
-    language="c++"
-)
+exts = [
+    Extension(
+    "parallel_sort",
+        sources=["parallel_sort/parallel_sort.pyx"],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        extra_compile_args=["-fopenmp"],
+        extra_link_args=["-liomp5", "-lpthread"],
+        include_dirs=[np.get_include()],
+        language="c++"
+    )
+]
 
 with open("README.md", "r") as f:
     long_desc = f.read()
 
 setup(
-    name="parallel-sort-csykes",
+    name="parallel-sort",
     version="0.0.1",
     author="Calvin Sykes",
     author_email="sykescalvin09@gmail.com",
@@ -29,14 +31,15 @@ setup(
     ],
     cmdclass={"build_ext": build_ext},
     description="Cython interface to GNU Parallel sorting routines",
-    ext_modules=cythonize(ext, language_level=3),
-    install_requires=["numpy", "pytest", "cython"],
+    ext_modules=cythonize(exts, language_level=3),
+    install_requires=["numpy"],
     long_description=long_desc,
     long_description_content_type="text/markdown",
     package_dir={"": "parallel_sort"},
     packages=find_packages(where="parallel_sort"),
     project_urls={"Bug Tracker": "https://github.com/calvin-sykes/cython_parallel_sort/issues"},
     python_requires=">=3.6",
-    setup_requires=["numpy", "cython"],
+    setup_requires=["cython", "numpy"],
+    tests_require=["pytest"],
     zip_safe=False
 )
